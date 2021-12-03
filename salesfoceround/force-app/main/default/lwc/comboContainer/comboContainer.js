ee36bd3754bf478848c0f2dc5728f530f9516e25
sergioavellar1906 <sergio.java.des@gmail.com>
2021-12-03 18:41:43 -0300

    Atualizando

---
+++ salesfoceround/force-app/main/default/lwc/comboContainer/comboContainer.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class ComboContainer extends LightningElement {}
---
+++ salesfoceround/force-app/main/default/lwc/jogadorContainer/jogadorContainer.js
@@ -0,0 +1,7 @@
+import { LightningElement, api } from 'lwc';
+
+export default class JogadorContainer extends LightningElement {
+    @api jogador;
+    @api index; 
+
+}
